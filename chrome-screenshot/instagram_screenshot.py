c053df27c75f204d67c1771fab05750deaaf2e11
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2020-04-15 09:45:34 +0700

    update isntagram_screenshot.py

--- chrome-screenshot/instagram_screenshot.py
+++ chrome-screenshot/instagram_screenshot.py
@@ -11,7 +11,7 @@ WINDOW_SIZE = "1440,2560"
 
 sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
@@ -27,7 +27,8 @@ def InstagramCrawler(keyword, out_path):
 
     cookies = loginInstagram(keyword)
     # ProfileScreenshot(keyword, out_path, cookies)
-    PhotoDownloader(keyword, out_path, cookies)
+    # PhotoDownloader(keyword, out_path, cookies)
+    PostFinder(keyword, out_path, cookies)
     driver.close()
 
 def loginInstagram(keyword):
@@ -123,6 +124,7 @@ def PhotoDownloader(keyword, out_path, cookies):
     print('opening page')
 
     driver.get(keywords_link)
+
     for cookie in cookies:
         driver.add_cookie(cookie)
     driver.refresh()
@@ -174,3 +176,70 @@ def PhotoDownloader(keyword, out_path, cookies):
 
     link_list = ScrapeLinks()
     OpenLinks()
+
+def PostFinder(keyword, out_path, cookies):
+    instagram_url = "https://www.instagram.com/"
+    keywords_link = instagram_url + keyword
+    if not keywords_link.startswith('http'):
+        raise Exception('URLs need to start with "http"') 
+
+    print('opening', keyword, 'page')
+
+    driver.get(keywords_link)
+
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    driver.refresh()
+
+    time.sleep(5)
+
+    def ScrapeLinks():
+        scroll_iterator = 0
+        height = 0
+        link_list = []
+
+        for i in range(50):
+            try:
+                photo_elements = driver.find_elements_by_tag_name('a')
+
+                for photos in photo_elements:
+                    post_link = str(photos.get_attribute('href'))
+
+                    if '/p/' in post_link:
+                        link_list.append(post_link)
+                        print(post_link, 'appended to links')
+
+                next_height = driver.execute_script("return document.body.scrollHeight")
+                if next_height == height:
+                    break
+                height = driver.execute_script("return document.body.scrollHeight")
+
+                driver.execute_script("window.scrollBy(0,2160)")
+                time.sleep(3)
+                scroll_iterator += 1
+            except:
+                pass
+        link_list = list(dict.fromkeys(link_list))
+        print(link_list, len(link_list))
+        return link_list
+    
+    def OpenLinks():
+        for index, links in enumerate(link_list):
+            driver.get(links)
+
+            time.sleep(2)
+
+            save_path = os.path.join(out_path, keyword)
+            try:
+                os.makedirs(save_path)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+            print('making Instagram screenshots for', keyword, (index+1))
+            driver.save_screenshot(os.path.join(save_path, str(timestamp)+'.png'))
+
+    link_list = ScrapeLinks()
+    OpenLinks()
+    time.sleep(5)
